bafac6912c075949859fe20dbec0e49724f339b2
jovi920204 <jovi920204@gmail.com>
2022-06-19 16:58:42 +0800

    commit
    mouse event

--- EquationDrawing/drawer.cpp
+++ EquationDrawing/drawer.cpp
@@ -9,11 +9,14 @@ Drawer::Drawer(QWidget *parent) :
     ui->setupUi(this);
     this->setGeometry(0,0,601,601);
     intervalNumber = 10;
+
     xMax = 10;
     xMin = -10;
     yMax = 10;
     yMin = -10;
 
+
+    setMouseTracking(true);
 }
 
 Drawer::~Drawer()
@@ -21,8 +24,16 @@ Drawer::~Drawer()
     delete ui;
 }
 
-void Drawer::changeRange(int xmax,int xmin, int ymax, int ymin){
+void Drawer::changeRange(double xmax,double xmin, double ymax, double ymin){
+//    qDebug() << xmax << " " << xmin << " " << ymax << " " << ymin;
+    if ((xmax - xmin) <= 8 || (ymax - ymin) <= 8){
+        return;
+    }
+    if ((xmax-xmin) >= 600 || (ymax - ymin) >= 600){
+        return;
+    }
     xMax = xmax, xMin = xmin, yMax = ymax, yMin = ymin;
+    Drawer::update();
 }
 
 void Drawer::addEquation(Widget *w){
@@ -41,37 +52,45 @@ void Drawer::receiveDeleteEquation(Widget *w){
 }
 
 void Drawer::paintEvent(QPaintEvent *event){
-    qDebug() << "paintEvent!!";
+//    qDebug() << "paintEvent!!";
     QPainter painter(this);
     int W = width();
     int H = height();
     int side = qMin(xMax-xMin,yMax-yMin);
     int x_Pro = 600/(xMax-xMin);
     int y_Pro = 600/(yMax-yMin);
-
+    int xRange = xMax - xMin;
+    int yRange = yMax - yMin;
     QRect rect(0, 0, 600, 600);
     painter.drawRect(rect);
 
     painter.setViewport(rect);
-    painter.setWindow(xMin,yMax,xMax*2,yMin*2);
+//    painter.setWindow(-xRange/2,yRange/2,xRange,-yRange);
+    painter.setWindow(xMin,yMax,xRange,-yRange);
 
     int interval = intervalNumber;
 
     // draw x and y axis
-    painter.setPen(QPen(Qt::black, 1/x_Pro+(0.1)*xMax/10));
+    painter.setPen(QPen(Qt::black, 1/x_Pro+(0.1)*xRange/10));
     painter.drawLine(xMin,0,xMax,0);
     painter.drawLine(0,yMin,0,yMax);
     // draw grid => default interval is 10 block;
     painter.setPen(QPen(Qt::black,1/x_Pro));
-    for (double x=xMin;x<=xMax;x+=(side/interval)){
-        painter.drawLine(x,yMin,x,yMax);
+    for (double x=0;x<=xMax;x+=(side/interval)){
+        painter.drawLine(QPointF(x,yMin),QPointF(x,yMax));
     }
-    for (double y=yMin;y<=yMax;y+=(side/interval)){
-        painter.drawLine(xMin,y,xMax,y);
+    for (double x=0;x>=xMin;x-=(side/interval)){
+        painter.drawLine(QPointF(x,yMin),QPointF(x,yMax));
+    }
+    for (double y=0;y<=yMax;y+=(side/interval)){
+        painter.drawLine(QPointF(xMin,y),QPointF(xMax,y));
+    }
+    for (double y=0;y>=yMin;y-=(side/interval)){
+        painter.drawLine(QPointF(xMin,y),QPointF(xMax,y));
     }
 
     //draw equation;
-    painter.setPen(QPen(Qt::blue,1/x_Pro+(0.1)*xMax/10));
+    painter.setPen(QPen(Qt::blue,1/x_Pro+(0.1)*xRange/2/10));
     double preX = xMin ,preY = qSin(xMin);
     double dot = (xMax-xMin)/500.f;
 
@@ -80,22 +99,12 @@ void Drawer::paintEvent(QPaintEvent *event){
         preX = x;
         preY = qSin(x);
     }
-
-    std::vector<double> xdot;
-    std::vector<double> ydot;
-    std::ifstream file("C:/Users/jovi9/Documents/code/EquationDrawing/EquationDrawing/EquationDrawing/output/output2.txt");
-    double x, y;
-    double prex, prey;
-    while(file.good()){
-        file >> x >> y;
-        xdot.push_back(x);
-        ydot.push_back(y);
-    }
-    prex = xdot[0], prey = ydot[0];
-    for (int i= 1; i < xdot.size(); i++){
-        if (prey == ydot[i]) continue;
-        painter.drawLine(QPointF(prex, prey),QPointF(xdot[i],ydot[i]));
-        prex = xdot[i], prey = ydot[i];
+    for (int x = xMin; x <= xMax ; x++){
+        for (int y = yMin ; y <= yMax ; y++){
+            if (x == y) {
+                painter.drawPoint(x,y);
+            }
+        }
     }
 
     // 一個for迴圈跑過所有的equationList
@@ -104,4 +113,51 @@ void Drawer::paintEvent(QPaintEvent *event){
     // 畫的步驟是 -> 帶入範圍 -> 生成出檔案 -> vector存檔案的x,y -> 跑vector中所有的點{ painter.drawLine }
 }
 
+void Drawer::mousePressEvent(QMouseEvent *event){
+    pressflag = true;
+}
+
+void Drawer::mouseMoveEvent(QMouseEvent *event){
+    int sensity = 600/(xMax-xMin);
+
+    if(pressflag){
+        QPointF temp;
+        QPointF nowMousePos = event->pos();
+        if (lastMousePos.isNull()){
+            lastMousePos = nowMousePos;
+        }
+        else{
+            temp = nowMousePos - lastMousePos;
+            qDebug() << temp;
+            changeRange(xMax-temp.x()/sensity,xMin-temp.x()/sensity,yMax+temp.y()/sensity,yMin+temp.y()/sensity);
+            lastMousePos = nowMousePos;
+        }
+
+    }
+}
+
+void Drawer::mouseReleaseEvent(QMouseEvent *event){
+    pressflag = false;
+    lastMousePos = QPoint();
+}
+
+void Drawer::wheelEvent(QWheelEvent *event){
+    QPoint numDegrees = event->angleDelta();
+    if (numDegrees.y() > 0){ // 向前滾動
+//        qDebug() << "front" << numDegrees.y();
+        this->zoomIn();
+    }
+    else {
+//        qDebug() << "back" << numDegrees.y();
+        this->zoomOut();
+    }
+}
+
+void Drawer::zoomIn(){
+    this->changeRange(xMax-1,xMin+1,yMax-1,yMin+1);
+}
+
+void Drawer::zoomOut(){
+    this->changeRange(xMax+1,xMin-1,yMax+1,yMin-1);
+}
 
--- EquationDrawing/drawer.h
+++ EquationDrawing/drawer.h
@@ -14,6 +14,8 @@
 #include <complex>
 #include <QFile>
 #include <QList>
+#include <QMouseEvent>
+#include <QWheelEvent>
 
 #include <map>
 #include <vector>
@@ -32,27 +34,36 @@ public:
     explicit Drawer(QWidget *parent = nullptr);
     ~Drawer();
 
-    void paintEvent(QPaintEvent *);
 
     int intervalNumber;
 
-    void changeRange(int xmax,int xmin, int ymax, int ymin);
+    void changeRange(double xmax,double xmin, double ymax, double ymin); // the scale only integer
 
     void addEquation(Widget *w);
 
+    void zoomIn();
+
+    void zoomOut();
+protected:
+    void paintEvent(QPaintEvent *);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void wheelEvent(QWheelEvent *event);
 
 private slots:
     void receiveAddEquation(Widget *);
     void receiveDeleteEquation(Widget *);
+
 private:
     Ui::Drawer *ui;
-    int xMax,xMin,yMax,yMin;
+    double xMax,xMin,yMax,yMin;
     QList<Widget*> equationList;
     // 當CreateButton新增時, 用成員函式新增Widget物件到equationLst裡面
     // visible或是delete也是調用成員函式完成狀態更新或是刪除
     std::map<QString,QString> table; // variable table
-
-
+    QPointF lastMousePos; // mouse current position
+    bool pressflag = false;
 };
 
 #endif // DRAWER_H
